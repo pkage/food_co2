7d9edd1ad512c7ad46e59716abdcff3df1f30627
Patrick Kage <patrick.r.kage@gmail.com>
2020-01-18 21:58:20 +0000

    add scanner component

--- frontend/src/js/components/App/App.js
+++ frontend/src/js/components/App/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {
+    useState
+} from 'react'
+import ScannerContainer from '../Scanner/ScannerContainer'
 
 const App = () => {
-    return (<h1> Hallo ! </h1>)
+    const [results, setResults] = useState([])
+
+    const addResult = res => setResults([res, ...results])
+
+    
+    return (
+        //        <Scanner onDetected={res => console.log(res)}/>
+        <ScannerContainer/>
+    )
 }
 
 export default App
