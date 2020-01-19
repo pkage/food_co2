0d6443b33efcc046987fa6ef92781570d8e98f93
Alexander Wasey <alex@wasey.net>
2020-01-19 06:03:44 +0000

    Added train mode for time

--- backend/train.py
+++ backend/train.py
@@ -1,3 +1,7 @@
 #Given in km convert to kg
 def calctrainfromdistance(distance): 
     return 0.041 * distance
+
+#Time given in minutes returned in kg
+def calctrainfromtime(time): 
+    return 0.041 * (time / 60.0) * 100.0
