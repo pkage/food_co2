650f0939e7e0b26e79489c99a28ddc4853e47035
Findlay Smith <findlayksmith@gmail.com>
2020-01-19 03:14:55 +0000

    bunch a shit idk

--- backend/suggestions.py
+++ backend/suggestions.py
@@ -1,71 +1,76 @@
 import json
 
 with open('backend/pollution.json') as f:
-  data = json.load(f)
+    data = json.load(f)
 
 
-#Will return a list of (original,[suggestions])
-def getsuggestions(ingredients): 
+# Will return a list of (original,[suggestions])
+def getsuggestions(ingredients):
     responses = list()
 
-    for ingredient in ingredients: 
+    for ingredient in ingredients:
         co2 = getco2(ingredient)
         categories = getcategories(ingredient)
         suggestions = mostsimilar(categories, co2)
         responses.append((ingredient, suggestions))
-    
+
     return responses
 
-def getcategories(name): 
-    for group in data: 
-        if name in group['keywords']: 
+
+def getcategories(name):
+    for group in data:
+        if name in group['keywords']:
             return group['categories']
     return []
 
-def getco2(name): 
-    for group in data: 
-        if name in group['keywords']: 
+
+def getco2(name):
+    for group in data:
+        if name in group['keywords']:
             return group['co2']
 
-#Will return the set of names for the most similar 
+# Will return the set of names for the most similar
+
+
 def mostsimilar(categories, co2):
     mostsim = 0.0
     currentsim = []
 
-    for item in data:    
-        if item['co2'] < co2: 
+    for item in data:
+        if item['co2'] < co2:
             try:
-                sim = similar(categories, item['categories']) 
-                if (sim > 0.5):                    
-                    if sim > mostsim: 
+                sim = similar(categories, item['categories'])
+                if (sim > 0.5):
+                    if sim > mostsim:
                         currentsim = item['keywords']
                         mostsim = sim
-                    elif sim == mostsim: 
+                    elif sim == mostsim:
                         currentsim = currentsim + item['keywords']
-                
-            except: 
+
+            except:
                 pass
 
     return currentsim
 
-def similar(original, new): 
-    hit = 0.0 
-    miss = 0.0 
 
-    for x in original: 
-        if x in new: 
-            hit += 1.0 
-        else: 
+def similar(original, new):
+    hit = 0.0
+    miss = 0.0
+
+    for x in original:
+        if x in new:
+            hit += 1.0
+        else:
             miss += 1.0
-    
-    for x in new: 
-        if x in original: 
-            hit += 1.0 
-        else: 
+
+    for x in new:
+        if x in original:
+            hit += 1.0
+        else:
             miss += 1.0
 
     return hit / (hit + miss)
 
 
-if(__name__== "__main__"): 
-    print(getsuggestions(["Asparagus"]))
+if(__name__ == "__main__"):
+    print(getsuggestions(["Asparagus", "Sunflower Seed Oil", "Walnuts", "Mashed Potato"]))
